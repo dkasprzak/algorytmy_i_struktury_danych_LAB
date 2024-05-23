d0ed4168edf1b4fc05439dbd4284a0933415eb60
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-05-23 18:24:44 +0200

    Task Allocation Problem

---
+++ TaskAllocationProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using TaskAllocationProblem;
+
+var (tasks, processors) = TaskAlocationGA.DataGenerator.GenerateData(10, 3);
+
+int populationSize = 100;
+int generations = 100;
+double mutationRate = 0.01;
+
+var bestChromosome = TaskAlocationGA.EvolutionaryAlgorithm.Run(tasks, processors, populationSize, generations, mutationRate);
+
+TaskAlocationGA.TaskAllocator.AssignTasks(tasks, processors, bestChromosome);
+
+TaskAlocationGA.TaskAllocator.DisplayResults(processors);
